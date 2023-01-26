2c1e9b9c21ef4506613d1df1d4cf3cdd67c7dcb4
JOSE PIERONI DOS SANTOS - STUDENT <G00390007@atu.ie>
2023-01-26 16:55:14 +0000

    Continue on runner2

---
+++ src/main/Runner2.java
@@ -0,0 +1,56 @@
+package main;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Scanner;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+public class Runner2 {
+
+	public static void main(String[] args) {
+		
+		MysqlDataSource mysqlDS = new MysqlDataSource();
+		mysqlDS.setURL("jdbc:mysql://localhost:3306/salespersondb");
+		mysqlDS.setUser("root");
+		mysqlDS.setPassword("root");
+		
+		try {
+			Connection conn = mysqlDS.getConnection();
+			Statement myStmt = conn.createStatement();
+			String query = "select sp.sid, sp.fname, sp.surname, sc.city, sc.commission from salesperson_table sp inner join salesperson_city_table sc on sp.sid = sc.sid;";
+
+			ResultSet rs = myStmt.executeQuery(query);
+			
+			Scanner in = new Scanner(System.in);
+			
+			System.out.println("Enter commission: ");
+			double userCommission = in.nextDouble();
+			
+			while (rs.next()) {
+				String sid = rs.getString("sid");
+				String fname = rs.getString("fname");
+				String lname = rs.getString("surname");
+				//String dob = rs.getString("dob");
+				//String city = rs.getString("city");
+				double commission = rs.getDouble("commission");
+				
+				if (userCommission >= commission) {
+					System.out.print("SID = " + sid + ", ");
+					System.out.print("First Name = " + fname + ", ");
+					System.out.print("Last Name = " + lname + ", ");
+					//System.out.print("DOB = " + dob + ", ");
+					//System.out.print("City = " + city + ", ");
+					System.out.println("Comission = " + commission + ", ");					
+				}
+			}
+			in.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
