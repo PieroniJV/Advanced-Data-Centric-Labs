f2dadf5d11507eb273d8846b68a949d12930b0a4
JOSE PIERONI DOS SANTOS - STUDENT <G00390007@atu.ie>
2023-01-26 16:03:44 +0000

    First exercise done

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -1,5 +1,6 @@
 package main;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,7 +9,9 @@ import com.mysql.cj.jdbc.MysqlDataSource;
 
 public class Runner {
 
-	public static void main(String[] args) throws SQLException {		
+	public static void main(String[] args) throws SQLException {	
+		
+		System.out.println(" hello");
 
 		MysqlDataSource mysqlDS = new MysqlDataSource();
 		Connection conn = mysqlDS.getConnection();
@@ -18,11 +21,24 @@ public class Runner {
 		mysqlDS.setPassword("root");
 
 		Statement myStmt = conn.createStatement();
-		String query = "SELECT * from  salesperson_table;";
+		String query = "SELECT * from  salesperson_table;";		
 		
-		myStmt.executeQuery(query);
-
+		ResultSet rs = myStmt.executeQuery(query);
 		
+		while (rs.next()) {
+			int sid = rs.getInt("sid");
+			String fname = rs.getString("fname");
+			String lname = rs.getString("surname");
+			Date dob = rs.getDate("dob");
+			String city = rs.getString("city");
+			String comission = rs.getString("comission");
+			
+			System.out.print("SID = " + sid + ", ");
+			System.out.println("First Name = " + fname + ", ");
+			System.out.println("Last Name = " + lname + ", ");
+			System.out.println("DOB = " + dob + ", ");
+			System.out.println("City = " + city + ", ");
+			System.out.println("Comission = " + comission + ", ");
+		}		
 	}
-
 }
