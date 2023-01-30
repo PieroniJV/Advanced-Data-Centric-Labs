afb6fcfc4378b3df2107ae6ea5f345f6d0af4df0
Jose Vitor Pieroni <vitorpieroni1@gmail.com>
2023-01-30 22:06:12 +0000

    Work on Runner 2
    Set up correct query and prints to screen

--- src/main/Runner1.java
+++ src/main/Runner1.java
@@ -8,49 +8,56 @@ import java.sql.SQLException;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
+// Question 1 Part 1
 public class Runner1 {
 
 	public static void main(String[] args) {
 
+		// Declaring MySql credentials
 		MysqlDataSource mysqlDS = new MysqlDataSource();
 		mysqlDS.setURL("jdbc:mysql://localhost:3306/salespersondb");
 		mysqlDS.setUser("root");
 		mysqlDS.setPassword("root");
-		
+
 		try {
+			// Trying to establish connection with MySql database
 			Connection conn = mysqlDS.getConnection();
 			Statement myStmt = conn.createStatement();
-			String query = "SELECT * from  salesperson_table;";
 
+			// Querying MySql
+			String query = "SELECT * from  salesperson_table;";
 			ResultSet rs = myStmt.executeQuery(query);
-			
+
+			// For Question 1 Part 2, User enters a commission value
 			Scanner in = new Scanner(System.in);
-			
 			System.out.println("Enter commission: ");
 			double userCommission = in.nextDouble();
-			
+
+			// Loop through Database results, For Q1P2, it only prints out values where
+			// commission is same or lower than the input
 			while (rs.next()) {
 				String sid = rs.getString("sid");
 				String fname = rs.getString("fname");
 				String lname = rs.getString("surname");
-				//String dob = rs.getString("dob");
-				//String city = rs.getString("city");
+				// String dob = rs.getString("dob");
+				// String city = rs.getString("city");
 				double commission = rs.getDouble("commission");
-				
+
+				// Q1P2 only
 				if (userCommission >= commission) {
 					System.out.print("SID = " + sid + ", ");
 					System.out.print("First Name = " + fname + ", ");
 					System.out.print("Last Name = " + lname + ", ");
-					//System.out.print("DOB = " + dob + ", ");
-					//System.out.print("City = " + city + ", ");
-					System.out.println("Comission = " + commission + ", ");					
+					// System.out.print("DOB = " + dob + ", ");
+					// System.out.print("City = " + city + ", ");
+					System.out.println("Comission = " + commission + ", ");
 				}
 			}
+			// Close Scanner
 			in.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
-	
-	}
-}
+		}
+
+	}// main
+}// class
--- src/main/Runner2.java
+++ src/main/Runner2.java
@@ -1,56 +1,50 @@
+
 package main;
+
 import java.sql.Connection;
 import java.sql.Statement;
-import java.util.Scanner;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import com.mysql.cj.jdbc.MysqlDataSource;
 
+// Question 2 Part 1
 public class Runner2 {
 
 	public static void main(String[] args) {
-		
+
 		MysqlDataSource mysqlDS = new MysqlDataSource();
-		mysqlDS.setURL("jdbc:mysql://localhost:3306/salespersondb");
+		mysqlDS.setURL("jdbc:mysql://localhost:3306/salespersondb2p2");
 		mysqlDS.setUser("root");
 		mysqlDS.setPassword("root");
-		
+
 		try {
 			Connection conn = mysqlDS.getConnection();
 			Statement myStmt = conn.createStatement();
-			String query = "select sp.sid, sp.fname, sp.surname, sc.city, sc.commission from salesperson_table sp inner join salesperson_city_table sc on sp.sid = sc.sid;";
+			String query = "SELECT sp.sid, sp.fname, sp.surname, ct.name, spc.commission FROM salesperson_table sp INNER JOIN salesperson_city_table spc ON sp.sid = spc.sid INNER JOIN city_table ct ON spc.cid = ct.cid;";
 
 			ResultSet rs = myStmt.executeQuery(query);
 			
-			Scanner in = new Scanner(System.in);
-			
-			System.out.println("Enter commission: ");
-			double userCommission = in.nextDouble();
-			
+			//continue here
 			while (rs.next()) {
 				String sid = rs.getString("sid");
 				String fname = rs.getString("fname");
 				String lname = rs.getString("surname");
-				//String dob = rs.getString("dob");
-				//String city = rs.getString("city");
-				double commission = rs.getDouble("commission");
+				// String dob = rs.getString("dob");
+				// String city = rs.getString("city");
 				
-				if (userCommission >= commission) {
-					System.out.print("SID = " + sid + ", ");
-					System.out.print("First Name = " + fname + ", ");
-					System.out.print("Last Name = " + lname + ", ");
-					//System.out.print("DOB = " + dob + ", ");
-					//System.out.print("City = " + city + ", ");
-					System.out.println("Comission = " + commission + ", ");					
-				}
+				System.out.print("SID = " + sid + ", ");
+				System.out.print("First Name = " + fname + ", ");
+				System.out.print("Last Name = " + lname + ", ");
+				// System.out.print("DOB = " + dob + ", ");
+				// System.out.print("City = " + city + ", ");
+
 			}
-			in.close();
+			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}
-	
-}
+
+	}// main
+
+}// class
+
